75462b7c06699040b73dd72bb46838dd8f8f3c72
Romain Andres <romain.andres@etu.unicaen.fr>
2023-08-02 18:46:15 +0200

    contacts

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, TextField, Button } from '@mui/material';
+import { Typography } from '@mui/material';
 import translations from '../../translations/fr.js';
 
 const Contact = () => {
@@ -7,18 +7,82 @@ const Contact = () => {
         <section id="contact" style={{ minHeight: '100vh' }}>
             <div style={{ padding: '20px' }}>
                 <Typography variant="h4">{translations['contact.me']}</Typography>
-                <p align="left">
-                    <a href="mailto:romain.andres@etu.unicaen.fr" target="_blank" rel="noreferrer"> 
-                        <img src="https://raw.githubusercontent.com/iconic/open-iconic/master/svg/envelope-closed.svg" alt="email" width="40" height="40" style={{ marginRight: '0.5em' }}/> 
-                    </a>
-                    <a href="https://www.linkedin.com/in/romain-andres-6b551b203/" target="_blank" rel="noreferrer"> 
-                        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linkedin/linkedin-original.svg" alt="linkedin" width="40" height="40" style={{ marginRight: '0.5em' }}/> 
-                    </a>
-                    <a href="https://github.com/VendenIX" target="_blank" rel="noreferrer"> 
-                        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg" alt="github" width="40" height="40" style={{ marginRight: '0.5em' }}/> 
-                    </a>
-                </p>
-                <p> Vous pouvez me joindre par mail : romain.andres@etu.unicaen.fr</p>
+                <ul>
+                <li>
+                        <strong>Email:</strong>{" "}
+                        <a
+                            href="mailto:romain.andres@etu.unicaen.fr"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            romain.andres@etu.unicaen.fr
+                        </a>
+                    </li>
+
+                    <li>
+                        <strong>LinkedIn:</strong>{" "}
+                        <a
+                            href="https://www.linkedin.com/in/romain-andres-6b551b203/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Romain Andres on LinkedIn
+                        </a>
+                    </li>
+
+                    <li>
+                        <strong>GitHub:</strong>{" "}
+                        <a
+                            href="https://github.com/VendenIX"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Romain Andres on GitHub
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div
+                style={{
+                    position: 'fixed',
+                    bottom: '10px',
+                    left: '10px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                }}
+            >
+                <a href="mailto:romain.andres@etu.unicaen.fr" target="_blank" rel="noreferrer">
+                    <img
+                        src="https://raw.githubusercontent.com/iconic/open-iconic/master/svg/envelope-closed.svg"
+                        alt="email"
+                        width="30"
+                        height="30"
+                        style={{ marginBottom: '0.5em' }}
+                    />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/romain-andres-6b551b203/"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img
+                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linkedin/linkedin-original.svg"
+                        alt="linkedin"
+                        width="30"
+                        height="30"
+                        style={{ marginBottom: '0.5em' }}
+                    />
+                </a>
+                <a href="https://github.com/VendenIX" target="_blank" rel="noreferrer">
+                    <img
+                        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg"
+                        alt="github"
+                        width="30"
+                        height="30"
+                        style={{ marginBottom: '0.5em' }}
+                    />
+                </a>
             </div>
         </section>
     );
