5ca795a74cce3e5dc155a63d7ec7e4fdf5ba64c0
Romain Andres <romain.andres@etu.unicaen.fr>
2023-08-02 22:20:51 +0200

    fix badges

--- src/pages/Projects/components/projet-badge.js
+++ src/pages/Projects/components/projet-badge.js
@@ -4,19 +4,27 @@ import { styled } from '@mui/system';
 import ListImages from './list-images';
 
 const BadgeCard = styled(Card)({
-  width: '45%', // Pour que la carte occupe toute la largeur du carrousel
-  margin: 'auto',
-  height: '100%',
-  overflow: 'hidden',
-  borderRadius: '1em',
-  // Ajoutez d'autres styles personnalisés ici si nécessaire
-  position: 'relative', // Ajout pour gérer le pseudo-élément :hover
-  transition: 'box-shadow 0.3s', // Animation de la surbrillance
-  '&:hover': {
-    cursor: 'pointer', // Curseur de la souris en main pointer au survol
-    boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)', // Surbrillance de l'ombre au survol
-  },
-});
+    width: '45%', // Pour que la carte occupe toute la largeur du carrousel
+    margin: 'auto',
+    height: '100%',
+    overflow: 'hidden',
+    borderRadius: '1em',
+    // Ajoutez d'autres styles personnalisés ici si nécessaire
+    position: 'relative', // Ajout pour gérer le pseudo-élément :hover
+    transition: 'box-shadow 0.3s', // Animation de la surbrillance
+    '&:hover': {
+      cursor: 'pointer', // Curseur de la souris en main pointer au survol
+      boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)', // Surbrillance de l'ombre au survol
+    },
+    '@media (max-width:800px)': {
+      width: '80%', 
+    },
+    '@media (max-width:400px)': {
+      width: '200%', 
+      margin: '-5em'
+    }
+  });
+  
 
 const TitleWrapper = styled('div')({
   display: 'flex',
