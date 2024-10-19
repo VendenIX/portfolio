75a06d572effa4b7e02d5d4d747246e763118a5d
Romain ANDRES <romain.andres@etu.unicaen.fr>
2024-10-19 12:53:20 +0200

    ajout bouton de téléchargement

--- src/pages/Curriculum-Vitae/index.js
+++ src/pages/Curriculum-Vitae/index.js
@@ -1,7 +1,8 @@
 // src/pages/Curriculum-Vitae/index.js
 
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Button } from '@mui/material';
+import DownloadIcon from '@mui/icons-material/Download';
 
 const CvViewer = () => {
     return (
@@ -20,6 +21,19 @@ const CurriculumVitae = () => {
                 <Typography variant="h4" style={{ marginBottom: '1em' }}>Curriculum Vitae</Typography>
                 <CvViewer />
             </div>
+            {/* Bouton pour Télécharger le CV */}
+            <Button
+            variant="contained"
+            color="primary"
+            startIcon={<DownloadIcon />}
+            href="https://raw.githubusercontent.com/VendenIX/curriculum_vitae/main/Curriculum_vitae.pdf"
+            download
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ margin: '1em 0' }}
+            >
+                Télécharger le CV
+            </Button>
         </section>
     );
 };
