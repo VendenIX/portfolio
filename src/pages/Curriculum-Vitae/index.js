2723b2b48a4b42f8546f2b0afea74634df15fb47
Romain ANDRES <romain.andres@etu.unicaen.fr>
2024-10-19 18:33:07 +0200

    cv better

--- src/pages/Curriculum-Vitae/index.js
+++ src/pages/Curriculum-Vitae/index.js
@@ -1,5 +1,3 @@
-// src/pages/Curriculum-Vitae/index.js
-
 import DownloadIcon from '@mui/icons-material/Download';
 import { Button, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
@@ -7,7 +5,7 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-const CvViewer = () => {
+const CvViewer = ({ setPdfHeight }) => {
   const [numPages, setNumPages] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -22,26 +20,34 @@ const CvViewer = () => {
     setNumPages(numPages);
   };
 
-  // Cycle entre les pages toutes les 5 secondes
+  const onPageLoadSuccess = (page) => {
+    const { height } = page.getViewport({ scale: 1 });
+    console.log(`Hauteur de la page : ${height}px`);
+    // convertir la hauteur en `em`, en supposant que 1em = 16px
+    setPdfHeight(height / 16);
+  };
+
+  // cycle entre les pages toutes les 5 secondes
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentPage((prevPage) => (prevPage % 2) + 1);
     }, 5000);
 
-    // Nettoyage de l'intervalle quand le composant se démonte
+    // nettoyage de l'intervalle quand le composant se démonte
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <div style={{ width: '60%', padding: '0 20%', marginBottom: '20vh' }}>
+      <div style={{ width: '60%', padding: '0 20%', marginBottom: '20vh', paddingRight: '25em' }}>
         <Document
           file={filePath}
           onLoadSuccess={onDocumentLoadSuccess}
         >
           <Page
             pageNumber={currentPage}
-            width={window.innerWidth * 0.6} // Ajuste la largeur à 60% de la largeur totale
+            width={window.innerWidth * 0.6} // largeur à 60% de la largeur totale
+            onLoadSuccess={onPageLoadSuccess} 
           />
         </Document>
       </div>
@@ -51,25 +57,26 @@ const CvViewer = () => {
 
 const CurriculumVitae = () => {
   const localDownloadPath = `${window.location.origin}/portfolio/Curriculum_vitae.pdf`;
+  const [pdfHeightEm, setPdfHeightEm] = useState(0);
 
   return (
-    <section id="cv" style={{ padding: '5em', minHeight: '145vh' }}>
+    <section id="cv" style={{ padding: '5em', minHeight: `${pdfHeightEm * 1.50}em` }}>
       <div>
         <Typography variant="h4" style={{ marginBottom: '1em' }}>Curriculum Vitae</Typography>
-              {/* Bouton pour Télécharger le CV */}
-      <Button
-        variant="contained"
-        color="primary"
-        startIcon={<DownloadIcon />}
-        href={localDownloadPath} // Chemin local pour le téléchargement
-        download
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{ margin: '1em 0' }}
-      >
-        Télécharger le CV
-      </Button>
-        <CvViewer />
+              {/* bouton pour Télécharger le CV */}
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<DownloadIcon />}
+          href={localDownloadPath} // path local pour le téléchargement
+          download
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ margin: '1em 0' }}
+        >
+          Télécharger le CV
+        </Button>
+        <CvViewer setPdfHeight={setPdfHeightEm} />
       </div>
     </section>
   );
